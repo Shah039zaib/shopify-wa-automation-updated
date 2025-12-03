88098dc9b65b36b7f355bfbcaa49348c6f3c048a
Shah039zaib <125696406+Shah039zaib@users.noreply.github.com>
2025-12-03 10:59:26 +0500

    Update App.tsx

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,16 +1,16 @@
-// admin/src/App.tsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import WhatsAppControl from "./pages/WhatsAppControl";
 import AuthGuard from "./AuthGuard";
-import MainApp from "./MainApp";
 
-export default function App() {
+export default function App(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<AuthGuard><MainApp /></AuthGuard>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/*" element={<AuthGuard><Dashboard/></AuthGuard>} />
       </Routes>
     </BrowserRouter>
   );
